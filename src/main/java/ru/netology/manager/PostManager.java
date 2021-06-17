b457b9451153ce1f46c63ca176f00eaed46f2181
Vago Grigoryan <Vago_1989@mail.ru>
2021-06-17 19:12:09 +0300

    manag

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    private Post[]posts;
+
+    public Post [] searchPost(int ownerId, String domain, String query, int count, int offset){ //Позволяет искать записи на стене в соответствии с заданными критериями.
+
+        return posts;
+    }
+    public void deletePost (int ownerId, int postId){ //Удаляет запись со стены.
+        return;
+    }
+
+}
