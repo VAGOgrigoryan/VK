82e4669ce0c7e9f3b4190f43f5c6f8f43140d677
Vago Grigoryan <Vago_1989@mail.ru>
2021-06-17 18:55:25 +0300

    v2

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,14 +1,35 @@
 package ru.netology.domain;
 
 public class Post {
-    public int id;
-    public int dateOfPlacement;
-    public int numberOfViews;
-    public String postTitle;
-    public String postText;
-    public String imageURL;
+    private int id;
+    private int dateOfPlacement;
+    private String postTitle;
+    private String postText;
     public Comments comments;
-    public Likes likes;
-    public Reposts reposts;
+    private Likes likes;
+    private Reposts reposts;
+    private int idOwner;
+    private int idFrom;
+    private int byCreate;
+    private int idReplyOwner;
+    private int idReplyPost;
+    private int onlyFriends;
+    private String copyright;
+    private ViewsInfo viewsInfo;
+    private String typePost;
+    private PostSource postSource;
+    private Geo geo;
+    private int idSigner;
+    private boolean canFix;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean canCopy;
+    private int isPinned;
+    private int idOwnerGive;
+    private int idOwnerLike;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int idPostponed;
+
 
 }
